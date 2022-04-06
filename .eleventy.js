3abe3130a49220437a26ace753451ab204454a66
Peter deHaan <peter@deseloper.com>
2022-04-05 22:48:52 -0700

    FIRST BLOOD

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+module.exports = function (eleventyConfig) {
+  // OPTION 1
+  eleventyConfig.addFilter("excludeByFrontMatter", function (collection=[], frontMatterItem="") {
+    if (!frontMatterItem) {
+      return collection;
+    }
+    return collection.filter(item => !item.data[frontMatterItem]);
+  });
+
+  // OPTION 2
+  eleventyConfig.addCollection("filtered_pages", function (collectionApi) {
+    const pages = collectionApi.getFilteredByTag("pages");
+    return [...pages].filter(page => page.data.draft !== true);
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "www",
+    }
+  };
+};
